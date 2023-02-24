eae4f0a859ca66d938fe874994ac36e582b1c6a1
hannahw1004 <75508874+hannahw1004@users.noreply.github.com>
2023-02-23 21:30:51 -0800

    edited table

--- src/Table.js
+++ src/Table.js
@@ -1,5 +1,45 @@
 import React, {useState} from 'react';
 
 export function Table(props){
-    
+    const {parseCrimeData, county, date} = props;
+    let crimeData = parseCrimeData;
+
+    if(county != ""){
+        crimeData = crimeData.filter((crime) => {
+            return crime.MCPP == county
+        })
+    }
+
+    if(date != null){
+        crimeData = crimeData.filter((crime) => {
+            return crime["Report DateTime"] == date
+        })
+    }
+
+    let tableRow = crimeData.map((crime) => {
+        
+        return(
+            <tr>
+                <td>{crime["Report DateTIme"]}</td>
+                <td>{crime.MCPP}</td>
+                <td>{crime["Offense Parent Group"]}</td>
+                <td>{crime.Offense}</td>
+            </tr>
+        )
+    })
+
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>County</th>
+                    <th>Offense Group</th>
+                    <th>Offense</th>
+                </tr>
+            </thead>
+            <tbody>{tableRow}</tbody>
+        </table>
+    )
 }
